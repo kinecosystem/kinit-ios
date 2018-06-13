c79d9fd43529115cde3ebe80d1237c05b179aab5
Natan Rolnik <natanrolnik@gmail.com>
2018-06-13 16:52:04 +0300

    Adjusts URL for jpg images as well

--- KinWallet/Extensions/URL+KinWallet.swift
+++ KinWallet/Extensions/URL+KinWallet.swift
@@ -11,7 +11,14 @@ extension URL {
     func kinImagePathAdjustedForDevice() -> URL {
         var components = path.components(separatedBy: "/")
 
-        guard var last = components.last, last.hasSuffix(".png") else {
+        guard var last = components.last else {
+            return self
+        }
+
+        let isPng = last.hasSuffix(".png")
+        let isJpg = last.hasSuffix(".jpg")
+
+        guard isPng || isJpg else {
             return self
         }
 
@@ -19,8 +26,18 @@ extension URL {
         components.insert("ios", at: components.count - 1)
         components.removeLast()
 
-        let toReplace = (UIScreen.main.scale > 2 ? "@3x.png" : "@2x.png")
-        last = last.replacingOccurrences(of: ".png", with: toReplace)
+        let newSuffix: String
+        let fileExtension: String
+
+        if isPng {
+            newSuffix = (UIScreen.main.scale > 2 ? "@3x.png" : "@2x.png")
+            fileExtension = ".png"
+        } else {
+            newSuffix = (UIScreen.main.scale > 2 ? "@3x.jpg" : "@2x.jpg")
+            fileExtension = ".jpg"
+        }
+
+        last = last.replacingOccurrences(of: fileExtension, with: newSuffix)
         components.append(last)
 
         urlComponents.scheme = scheme
