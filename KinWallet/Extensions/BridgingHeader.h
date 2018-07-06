d60a269894647250baabefc9ae3e3c9713e5ca5c
Wim Hollebrandse <wim@wimdows.com>
2018-07-06 11:56:38 +0100

    Ensured contact phone number won't be stored in the clear on server end by applying a basic and straightforward SHA256 hash. Used after verification as well as locating other users to send Kin to.

---
+++ KinWallet/Extensions/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  KinWallet
+//
+//  Copyright © 2018 KinFoundation. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <CommonCrypto/CommonHMAC.h>
+
+#endif /* BridgingHeader_h */
