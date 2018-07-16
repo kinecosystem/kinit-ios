10858d6b81f81cf052876983ddb911c6bf85e014
Natan Rolnik <natanrolnik@gmail.com>
2018-07-16 13:02:46 +0300

    Updates Generated Files/* to use updated SwiftGen

--- KinWallet/Generated Files/ImageAssets.swift
+++ KinWallet/Generated Files/ImageAssets.swift
@@ -2,24 +2,24 @@
 
 #if os(OSX)
   import AppKit.NSImage
-  typealias AssetColorTypeAlias = NSColor
-  typealias Image = NSImage
+  internal typealias AssetColorTypeAlias = NSColor
+  internal typealias Image = NSImage
 #elseif os(iOS) || os(tvOS) || os(watchOS)
   import UIKit.UIImage
-  typealias AssetColorTypeAlias = UIColor
-  typealias Image = UIImage
+  internal typealias AssetColorTypeAlias = UIColor
+  internal typealias Image = UIImage
 #endif
 
 // swiftlint:disable superfluous_disable_command
 // swiftlint:disable file_length
 
 @available(*, deprecated, renamed: "ImageAsset")
-typealias AssetType = ImageAsset
+internal typealias AssetType = ImageAsset
 
-struct ImageAsset {
-  fileprivate var name: String
+internal struct ImageAsset {
+  internal fileprivate(set) var name: String
 
-  var image: Image {
+  internal var image: Image {
     let bundle = Bundle(for: BundleToken.self)
     #if os(iOS) || os(tvOS)
     let image = Image(named: name, in: bundle, compatibleWith: nil)
@@ -33,80 +33,80 @@ struct ImageAsset {
   }
 }
 
-struct ColorAsset {
-  fileprivate var name: String
+internal struct ColorAsset {
+  internal fileprivate(set) var name: String
 
   @available(iOS 11.0, tvOS 11.0, watchOS 4.0, OSX 10.13, *)
-  var color: AssetColorTypeAlias {
+  internal var color: AssetColorTypeAlias {
     return AssetColorTypeAlias(asset: self)
   }
 }
 
 // swiftlint:disable identifier_name line_length nesting type_body_length type_name
-enum Asset {
-  static let buttonStroke = ImageAsset(name: "ButtonStroke")
-  static let closeXButton = ImageAsset(name: "CloseXButton")
-  static let closeXButtonDarkGray = ImageAsset(name: "CloseXButtonDarkGray")
-  static let confettiDiamond = ImageAsset(name: "ConfettiDiamond")
-  static let confettiNormal = ImageAsset(name: "ConfettiNormal")
-  static let confettiStar = ImageAsset(name: "ConfettiStar")
-  static let confettiTriangle = ImageAsset(name: "ConfettiTriangle")
-  static let doneFireworks = ImageAsset(name: "DoneFireworks")
-  static let doneSign = ImageAsset(name: "DoneSign")
-  static let dualImageSeparator = ImageAsset(name: "DualImageSeparator")
-  static let emptyHistoryCoupon = ImageAsset(name: "EmptyHistoryCoupon")
-  static let errorSign = ImageAsset(name: "ErrorSign")
-  static let floorShadow = ImageAsset(name: "FloorShadow")
-  static let historyCouponIcon = ImageAsset(name: "HistoryCouponIcon")
-  static let historyCouponLine = ImageAsset(name: "HistoryCouponLine")
-  static let historyKinIcon = ImageAsset(name: "HistoryKinIcon")
-  static let historyShareButton = ImageAsset(name: "HistoryShareButton")
-  static let imageOnlyAnswerBackground = ImageAsset(name: "ImageOnlyAnswerBackground")
-  static let imageTextAnswerBackground = ImageAsset(name: "ImageTextAnswerBackground")
-  static let imageTextAnswerBackgroundSelected = ImageAsset(name: "ImageTextAnswerBackgroundSelected")
-  static let kinCoin = ImageAsset(name: "KinCoin")
-  static let kinLogoSplash = ImageAsset(name: "KinLogoSplash")
-  static let kinReward = ImageAsset(name: "KinReward")
-  static let kinTaskReward = ImageAsset(name: "KinTaskReward")
-  static let kinTaskTime = ImageAsset(name: "KinTaskTime")
-  static let kinTransactionCoins = ImageAsset(name: "KinTransactionCoins")
-  static let moreTabIcon = ImageAsset(name: "MoreTabIcon")
-  static let multipleAnswerBackground = ImageAsset(name: "MultipleAnswerBackground")
-  static let multipleAnswerBackgroundSelected = ImageAsset(name: "MultipleAnswerBackgroundSelected")
-  static let multipleAnswerCheckmark = ImageAsset(name: "MultipleAnswerCheckmark")
-  static let multipleAnswerPlus = ImageAsset(name: "MultipleAnswerPlus")
-  static let nextTask = ImageAsset(name: "NextTask")
-  static let noInternetIllustration = ImageAsset(name: "NoInternetIllustration")
-  static let noOffers = ImageAsset(name: "NoOffers")
-  static let offerCardShadow = ImageAsset(name: "OfferCardShadow")
-  static let offerCardTopLeftCorner = ImageAsset(name: "OfferCardTopLeftCorner")
-  static let patternPlaceholder = ImageAsset(name: "PatternPlaceholder")
-  static let paymentDelay = ImageAsset(name: "PaymentDelay")
-  static let progressViewGradient = ImageAsset(name: "ProgressViewGradient")
-  static let progressViewTrack = ImageAsset(name: "ProgressViewTrack")
-  static let recordsLedger = ImageAsset(name: "RecordsLedger")
-  static let redeemBubble = ImageAsset(name: "RedeemBubble")
-  static let smallHeart = ImageAsset(name: "SmallHeart")
-  static let sowingIllustration = ImageAsset(name: "SowingIllustration")
-  static let splashScreenBackground = ImageAsset(name: "SplashScreenBackground")
-  static let tabBarBalance = ImageAsset(name: "TabBarBalance")
-  static let tabBarEarn = ImageAsset(name: "TabBarEarn")
-  static let tabBarMore = ImageAsset(name: "TabBarMore")
-  static let tabBarSpend = ImageAsset(name: "TabBarSpend")
-  static let textAnswerShape = ImageAsset(name: "TextAnswerShape")
-  static let textAnswerShapeSelected = ImageAsset(name: "TextAnswerShapeSelected")
-  static let timer = ImageAsset(name: "Timer")
-  static let transferFireworks = ImageAsset(name: "TransferFireworks")
-  static let walletCreationFailed = ImageAsset(name: "WalletCreationFailed")
-  static let welcomeTutorial1 = ImageAsset(name: "WelcomeTutorial1")
-  static let welcomeTutorial2 = ImageAsset(name: "WelcomeTutorial2")
-  static let welcomeTutorial3 = ImageAsset(name: "WelcomeTutorial3")
-  static let whiteCheckmark = ImageAsset(name: "WhiteCheckmark")
+internal enum Asset {
+  internal static let buttonStroke = ImageAsset(name: "ButtonStroke")
+  internal static let closeXButton = ImageAsset(name: "CloseXButton")
+  internal static let closeXButtonDarkGray = ImageAsset(name: "CloseXButtonDarkGray")
+  internal static let confettiDiamond = ImageAsset(name: "ConfettiDiamond")
+  internal static let confettiNormal = ImageAsset(name: "ConfettiNormal")
+  internal static let confettiStar = ImageAsset(name: "ConfettiStar")
+  internal static let confettiTriangle = ImageAsset(name: "ConfettiTriangle")
+  internal static let doneFireworks = ImageAsset(name: "DoneFireworks")
+  internal static let doneSign = ImageAsset(name: "DoneSign")
+  internal static let dualImageSeparator = ImageAsset(name: "DualImageSeparator")
+  internal static let emptyHistoryCoupon = ImageAsset(name: "EmptyHistoryCoupon")
+  internal static let errorSign = ImageAsset(name: "ErrorSign")
+  internal static let floorShadow = ImageAsset(name: "FloorShadow")
+  internal static let historyCouponIcon = ImageAsset(name: "HistoryCouponIcon")
+  internal static let historyCouponLine = ImageAsset(name: "HistoryCouponLine")
+  internal static let historyKinIcon = ImageAsset(name: "HistoryKinIcon")
+  internal static let historyShareButton = ImageAsset(name: "HistoryShareButton")
+  internal static let imageOnlyAnswerBackground = ImageAsset(name: "ImageOnlyAnswerBackground")
+  internal static let imageTextAnswerBackground = ImageAsset(name: "ImageTextAnswerBackground")
+  internal static let imageTextAnswerBackgroundSelected = ImageAsset(name: "ImageTextAnswerBackgroundSelected")
+  internal static let kinCoin = ImageAsset(name: "KinCoin")
+  internal static let kinLogoSplash = ImageAsset(name: "KinLogoSplash")
+  internal static let kinReward = ImageAsset(name: "KinReward")
+  internal static let kinTaskReward = ImageAsset(name: "KinTaskReward")
+  internal static let kinTaskTime = ImageAsset(name: "KinTaskTime")
+  internal static let kinTransactionCoins = ImageAsset(name: "KinTransactionCoins")
+  internal static let moreTabIcon = ImageAsset(name: "MoreTabIcon")
+  internal static let multipleAnswerBackground = ImageAsset(name: "MultipleAnswerBackground")
+  internal static let multipleAnswerBackgroundSelected = ImageAsset(name: "MultipleAnswerBackgroundSelected")
+  internal static let multipleAnswerCheckmark = ImageAsset(name: "MultipleAnswerCheckmark")
+  internal static let multipleAnswerPlus = ImageAsset(name: "MultipleAnswerPlus")
+  internal static let nextTask = ImageAsset(name: "NextTask")
+  internal static let noInternetIllustration = ImageAsset(name: "NoInternetIllustration")
+  internal static let noOffers = ImageAsset(name: "NoOffers")
+  internal static let offerCardShadow = ImageAsset(name: "OfferCardShadow")
+  internal static let offerCardTopLeftCorner = ImageAsset(name: "OfferCardTopLeftCorner")
+  internal static let patternPlaceholder = ImageAsset(name: "PatternPlaceholder")
+  internal static let paymentDelay = ImageAsset(name: "PaymentDelay")
+  internal static let progressViewGradient = ImageAsset(name: "ProgressViewGradient")
+  internal static let progressViewTrack = ImageAsset(name: "ProgressViewTrack")
+  internal static let recordsLedger = ImageAsset(name: "RecordsLedger")
+  internal static let redeemBubble = ImageAsset(name: "RedeemBubble")
+  internal static let smallHeart = ImageAsset(name: "SmallHeart")
+  internal static let sowingIllustration = ImageAsset(name: "SowingIllustration")
+  internal static let splashScreenBackground = ImageAsset(name: "SplashScreenBackground")
+  internal static let tabBarBalance = ImageAsset(name: "TabBarBalance")
+  internal static let tabBarEarn = ImageAsset(name: "TabBarEarn")
+  internal static let tabBarMore = ImageAsset(name: "TabBarMore")
+  internal static let tabBarSpend = ImageAsset(name: "TabBarSpend")
+  internal static let textAnswerShape = ImageAsset(name: "TextAnswerShape")
+  internal static let textAnswerShapeSelected = ImageAsset(name: "TextAnswerShapeSelected")
+  internal static let timer = ImageAsset(name: "Timer")
+  internal static let transferFireworks = ImageAsset(name: "TransferFireworks")
+  internal static let walletCreationFailed = ImageAsset(name: "WalletCreationFailed")
+  internal static let welcomeTutorial1 = ImageAsset(name: "WelcomeTutorial1")
+  internal static let welcomeTutorial2 = ImageAsset(name: "WelcomeTutorial2")
+  internal static let welcomeTutorial3 = ImageAsset(name: "WelcomeTutorial3")
+  internal static let whiteCheckmark = ImageAsset(name: "WhiteCheckmark")
 
   // swiftlint:disable trailing_comma
-  static let allColors: [ColorAsset] = [
+  internal static let allColors: [ColorAsset] = [
   ]
-  static let allImages: [ImageAsset] = [
+  internal static let allImages: [ImageAsset] = [
     buttonStroke,
     closeXButton,
     closeXButtonDarkGray,
@@ -168,11 +168,11 @@ enum Asset {
   ]
   // swiftlint:enable trailing_comma
   @available(*, deprecated, renamed: "allImages")
-  static let allValues: [AssetType] = allImages
+  internal static let allValues: [AssetType] = allImages
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
-extension Image {
+internal extension Image {
   @available(iOS 1.0, tvOS 1.0, watchOS 1.0, *)
   @available(OSX, deprecated,
     message: "This initializer is unsafe on macOS, please use the ImageAsset.image property")
@@ -188,7 +188,7 @@ extension Image {
   }
 }
 
-extension AssetColorTypeAlias {
+internal extension AssetColorTypeAlias {
   @available(iOS 11.0, tvOS 11.0, watchOS 4.0, OSX 10.13, *)
   convenience init!(asset: ColorAsset) {
     let bundle = Bundle(for: BundleToken.self)
--- KinWallet/Generated Files/Storyboards.swift
+++ KinWallet/Generated Files/Storyboards.swift
@@ -7,22 +7,22 @@ import UIKit
 // swiftlint:disable superfluous_disable_command
 // swiftlint:disable file_length
 
-protocol StoryboardType {
+internal protocol StoryboardType {
   static var storyboardName: String { get }
 }
 
-extension StoryboardType {
+internal extension StoryboardType {
   static var storyboard: UIStoryboard {
     let name = self.storyboardName
     return UIStoryboard(name: name, bundle: Bundle(for: BundleToken.self))
   }
 }
 
-struct SceneType<T: Any> {
-  let storyboard: StoryboardType.Type
-  let identifier: String
+internal struct SceneType<T: Any> {
+  internal let storyboard: StoryboardType.Type
+  internal let identifier: String
 
-  func instantiate() -> T {
+  internal func instantiate() -> T {
     let identifier = self.identifier
     guard let controller = storyboard.storyboard.instantiateViewController(withIdentifier: identifier) as? T else {
       fatalError("ViewController '\(identifier)' is not of the expected class \(T.self).")
@@ -31,10 +31,10 @@ struct SceneType<T: Any> {
   }
 }
 
-struct InitialSceneType<T: Any> {
-  let storyboard: StoryboardType.Type
+internal struct InitialSceneType<T: Any> {
+  internal let storyboard: StoryboardType.Type
 
-  func instantiate() -> T {
+  internal func instantiate() -> T {
     guard let controller = storyboard.storyboard.instantiateInitialViewController() as? T else {
       fatalError("ViewController is not of the expected class \(T.self).")
     }
@@ -42,9 +42,9 @@ struct InitialSceneType<T: Any> {
   }
 }
 
-protocol SegueType: RawRepresentable { }
+internal protocol SegueType: RawRepresentable { }
 
-extension UIViewController {
+internal extension UIViewController {
   func perform<S: SegueType>(segue: S, sender: Any? = nil) where S.RawValue == String {
     let identifier = segue.rawValue
     performSegue(withIdentifier: identifier, sender: sender)
@@ -52,72 +52,72 @@ extension UIViewController {
 }
 
 // swiftlint:disable explicit_type_interface identifier_name line_length type_body_length type_name
-enum StoryboardScene {
-  enum Earn: StoryboardType {
-    static let storyboardName = "Earn"
+internal enum StoryboardScene {
+  internal enum Earn: StoryboardType {
+    internal static let storyboardName = "Earn"
 
-    static let earnKinAnimationViewController = SceneType<EarnKinAnimationViewController>(storyboard: Earn.self, identifier: "EarnKinAnimationViewController")
+    internal static let earnKinAnimationViewController = SceneType<EarnKinAnimationViewController>(storyboard: Earn.self, identifier: "EarnKinAnimationViewController")
 
-    static let surveyDoneViewController = SceneType<SurveyDoneViewController>(storyboard: Earn.self, identifier: "SurveyDoneViewController")
+    internal static let surveyDoneViewController = SceneType<SurveyDoneViewController>(storyboard: Earn.self, identifier: "SurveyDoneViewController")
 
-    static let surveyInfoViewController = SceneType<SurveyInfoViewController>(storyboard: Earn.self, identifier: "SurveyInfoViewController")
+    internal static let surveyInfoViewController = SceneType<SurveyInfoViewController>(storyboard: Earn.self, identifier: "SurveyInfoViewController")
 
-    static let taskCompletedViewController = SceneType<TaskCompletedViewController>(storyboard: Earn.self, identifier: "TaskCompletedViewController")
+    internal static let taskCompletedViewController = SceneType<TaskCompletedViewController>(storyboard: Earn.self, identifier: "TaskCompletedViewController")
 
-    static let transferringKinViewController = SceneType<TransferringKinViewController>(storyboard: Earn.self, identifier: "TransferringKinViewController")
+    internal static let transferringKinViewController = SceneType<TransferringKinViewController>(storyboard: Earn.self, identifier: "TransferringKinViewController")
   }
-  enum LaunchScreen: StoryboardType {
-    static let storyboardName = "LaunchScreen"
+  internal enum LaunchScreen: StoryboardType {
+    internal static let storyboardName = "LaunchScreen"
 
-    static let initialScene = InitialSceneType<UIViewController>(storyboard: LaunchScreen.self)
+    internal static let initialScene = InitialSceneType<UIViewController>(storyboard: LaunchScreen.self)
 
-    static let splashScreenViewController = SceneType<UIViewController>(storyboard: LaunchScreen.self, identifier: "SplashScreenViewController")
+    internal static let splashScreenViewController = SceneType<UIViewController>(storyboard: LaunchScreen.self, identifier: "SplashScreenViewController")
   }
-  enum Main: StoryboardType {
-    static let storyboardName = "Main"
+  internal enum Main: StoryboardType {
+    internal static let storyboardName = "Main"
 
-    static let accountReadyViewController = SceneType<AccountReadyViewController>(storyboard: Main.self, identifier: "AccountReadyViewController")
+    internal static let accountReadyViewController = SceneType<AccountReadyViewController>(storyboard: Main.self, identifier: "AccountReadyViewController")
 
-    static let balancePagerTabViewController = SceneType<BalancePagerTabViewController>(storyboard: Main.self, identifier: "BalancePagerTabViewController")
+    internal static let balancePagerTabViewController = SceneType<BalancePagerTabViewController>(storyboard: Main.self, identifier: "BalancePagerTabViewController")
 
-    static let balanceViewController = SceneType<BalanceViewController>(storyboard: Main.self, identifier: "BalanceViewController")
+    internal static let balanceViewController = SceneType<BalanceViewController>(storyboard: Main.self, identifier: "BalanceViewController")
 
-    static let noticeViewController = SceneType<NoticeViewController>(storyboard: Main.self, identifier: "NoticeViewController")
+    internal static let noticeViewController = SceneType<NoticeViewController>(storyboard: Main.self, identifier: "NoticeViewController")
 
-    static let offerWallNavigationController = SceneType<KinNavigationController>(storyboard: Main.self, identifier: "OfferWallNavigationController")
+    internal static let offerWallNavigationController = SceneType<KinNavigationController>(storyboard: Main.self, identifier: "OfferWallNavigationController")
 
-    static let offerWallViewController = SceneType<OfferWallViewController>(storyboard: Main.self, identifier: "OfferWallViewController")
+    internal static let offerWallViewController = SceneType<OfferWallViewController>(storyboard: Main.self, identifier: "OfferWallViewController")
 
-    static let phoneConfirmationViewController = SceneType<PhoneConfirmationViewController>(storyboard: Main.self, identifier: "PhoneConfirmationViewController")
+    internal static let phoneConfirmationViewController = SceneType<PhoneConfirmationViewController>(storyboard: Main.self, identifier: "PhoneConfirmationViewController")
 
-    static let phoneVerificationRequestViewController = SceneType<PhoneVerificationRequestViewController>(storyboard: Main.self, identifier: "PhoneVerificationRequestViewController")
+    internal static let phoneVerificationRequestViewController = SceneType<PhoneVerificationRequestViewController>(storyboard: Main.self, identifier: "PhoneVerificationRequestViewController")
 
-    static let rootTabBarController = SceneType<RootTabBarController>(storyboard: Main.self, identifier: "RootTabBarController")
+    internal static let rootTabBarController = SceneType<RootTabBarController>(storyboard: Main.self, identifier: "RootTabBarController")
 
-    static let surveyHomeViewController = SceneType<SurveyHomeViewController>(storyboard: Main.self, identifier: "SurveyHomeViewController")
+    internal static let surveyHomeViewController = SceneType<SurveyHomeViewController>(storyboard: Main.self, identifier: "SurveyHomeViewController")
 
-    static let surveyNavigationController = SceneType<KinNavigationController>(storyboard: Main.self, identifier: "SurveyNavigationController")
+    internal static let surveyNavigationController = SceneType<KinNavigationController>(storyboard: Main.self, identifier: "SurveyNavigationController")
 
-    static let welcomeViewController = SceneType<WelcomeViewController>(storyboard: Main.self, identifier: "WelcomeViewController")
+    internal static let welcomeViewController = SceneType<WelcomeViewController>(storyboard: Main.self, identifier: "WelcomeViewController")
   }
-  enum Spend: StoryboardType {
-    static let storyboardName = "Spend"
+  internal enum Spend: StoryboardType {
+    internal static let storyboardName = "Spend"
 
-    static let kinSentViewController = SceneType<KinSentViewController>(storyboard: Spend.self, identifier: "KinSentViewController")
+    internal static let kinSentViewController = SceneType<KinSentViewController>(storyboard: Spend.self, identifier: "KinSentViewController")
 
-    static let offerDetailsNavigationController = SceneType<UINavigationController>(storyboard: Spend.self, identifier: "OfferDetailsNavigationController")
+    internal static let offerDetailsNavigationController = SceneType<UINavigationController>(storyboard: Spend.self, identifier: "OfferDetailsNavigationController")
 
-    static let offerDetailsViewController = SceneType<OfferDetailsViewController>(storyboard: Spend.self, identifier: "OfferDetailsViewController")
+    internal static let offerDetailsViewController = SceneType<OfferDetailsViewController>(storyboard: Spend.self, identifier: "OfferDetailsViewController")
 
-    static let sendKinOfferActionViewController = SceneType<SendKinOfferActionViewController>(storyboard: Spend.self, identifier: "SendKinOfferActionViewController")
+    internal static let sendKinOfferActionViewController = SceneType<SendKinOfferActionViewController>(storyboard: Spend.self, identifier: "SendKinOfferActionViewController")
 
-    static let sendKinViewController = SceneType<SendKinViewController>(storyboard: Spend.self, identifier: "SendKinViewController")
+    internal static let sendKinViewController = SceneType<SendKinViewController>(storyboard: Spend.self, identifier: "SendKinViewController")
 
-    static let standardOfferActionViewController = SceneType<StandardOfferActionViewController>(storyboard: Spend.self, identifier: "StandardOfferActionViewController")
+    internal static let standardOfferActionViewController = SceneType<StandardOfferActionViewController>(storyboard: Spend.self, identifier: "StandardOfferActionViewController")
   }
 }
 
-enum StoryboardSegue {
+internal enum StoryboardSegue {
 }
 // swiftlint:enable explicit_type_interface identifier_name line_length type_body_length type_name
 
